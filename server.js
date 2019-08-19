2b4073d5f746b4988a989a9f9b208c87365e0325
Kelly Dickson <kelly@sightsource.net>
2019-08-18 22:14:57 -0400

    it works now?

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // server.js
 import express from 'express';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+import Person from './src/controllers/Person';
 
+dotenv.config();
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
-})
+// app.get('/', (req, res) => {
+//   return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+// })
+
+app.post('/', Person.create);
+app.get('/', Person.getAll);
+app.get(':id', Person.getOne);
+app.put(':id', Person.update);
 
 app.listen(3000)
 console.log('app running on port ', 3000);
--- src/controllers/Person.js
+++ src/controllers/Person.js
@@ -1,33 +1,74 @@
 import PersonModel from '../models/Person';
+import db from '../db';
 
 const Person = {
-    create(req, res) {
-        if (!req.body.firstName || !req.body.lastName) {
-            return res.status(400).send({ 'message': 'All fields are required'});
-        }
-        const person = PersonModel.create(req.body);
-        return res.status(201).send(person);
-    },
 
-    getAll(req, res) {
-        const people = PersonModel.findAll();
-        return res.status(200).send(people);
-    },
+  async create(req, res) {
+    const text = `INSERT INTO
+      people(first_name, middle_name, last_name, display_name)
+      VALUES($1, $2, $3, $4)
+      returning *`;
+    const values = [
+      req.body.first_name,
+      req.body.middle_name,
+      req.body.last_name,
+      req.body.display_name
+    ];
 
-    getOne(req, res) {
-        const person = PersonModel.findOne(req.params.id);
-        if (!person) {
-          return res.status(404).send({'message': 'person not found'});
-        }
-        return res.status(200).send(person);
-    },
+    try {
+      const {rows} = await db.query(text, values);
+      return res.status(201).send(rows[0]);
+    } catch(error) {
+      return res.status(400).send(error);
+    }
+  },
 
-    update(req, res) {
-        const person = PersonModel.findOne(req.params.id);
-        if (!person) {
-          return res.status(404).send({'message': 'person not found'});
-        }
-        const updatedPerson = PersonModel.update(req.params.id, req.body)
-        return res.status(200).send(updatedPerson);
-      },
+  async getAll(req, res) {
+    const findAllQuery = 'SELECT * FROM people';
+    try {
+      const { rows, rowCount } = await db.query(findAllQuery);
+      return res.status(200).send({ rows, rowCount });
+    }
+    catch(error) {
+      console.log(process.env.DATABASE_URL);
+      return res.status(400).send(error);
+    }
+  },
+
+  async getOne(req, res) {
+    const text = 'SELECT * FROM people WHERE id = $1';
+    try {
+      const { rows } = await db.query(text, [req.params.id]);
+      if (!rows[0]) {
+        return res.status(404).send({'message': 'person not found'});
+      }
+      return res.status(200).send(rows[0]);
+    } catch(error) {
+      return res.status(400).send(error)
+    }
+  },
+
+  async update(res, req) {
+    const findOneQuery = "SELECT * FROM people WHERE id = $1";
+    const updateOneQuery = `UPDATE people SET first_name = $1, middle_name
+      = $2, last_name = $3, display_name = $4 WHERE id = $5 returning *`;
+    try {
+      const { rows } = await db.query(findOneQuery, [req.params.id]);
+      if (!rows[0]) {
+        return res.status(404).send({'message': 'person not found'});
+      }
+      const values = [
+        req.body.first_name || rows[0].first_name,
+        req.body.middle_name || rows[0].middle_name,
+        req.body.last_name || rows[0].last_name,
+        req.params.id
+      ];
+      const response = await db.query(updateOneQuery, values);
+      return res.status(200).send(response.rows[0]);
+    } catch(err) {
+      return res.status(400).send(err)
+    }
+  }
 }
+
+export default Person;
